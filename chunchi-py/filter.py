9e5594655f2079bb5aa4cf8adfc04df0fc35f2b8
Agustin <toapantaagustin9c@gmail.com>
2025-02-22 17:27:27 -0500

    feat: :fire: Update filter.py

--- chunchi-py/filter.py
+++ chunchi-py/filter.py
@@ -8,6 +8,18 @@ data = pd.read_csv(file_path)
 filtro = data["Fecha y Hora de actualización"] == "lunes, 01 de marzo de 2021 – 20:02:50 Informe No. 10"
 fila_filtrada = data[filtro]
 
+# Convertir las columnas numéricas a tipo entero (int)
+columnas_numericas = [
+    "Fallecidos", "Heridos", "Viviendas destruidas", "Bien público afectado",
+    "Bien público destruido", "Puentes afectados", "Vías destruidas",
+    "Productores afectados por pérdidas agrícolas", "Productores afectados por pérdidas en animales",
+    "Viviendas afectadas", "Personas afectadas", "Personas damnificadas",
+    "Animales con afectación", "Animales muertos"
+]
+
+# Rellenar valores NaN con 0 y convertir a enteros
+fila_filtrada[columnas_numericas] = fila_filtrada[columnas_numericas].fillna(0).astype(int)
+
 # Guardar la fila filtrada en un nuevo archivo CSV (opcional)
 fila_filtrada.to_csv("fila_filtrada.csv", index=False)
 
